388ccd42be2c41c6678fb55ece1677a211512995
kyle <altck8@gmail.com>
2016-02-15 09:23:20 -0800

    add vinyl-source-stream

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
+var concat      = require('gulp-concat');
+var source      = require('vinyl-source-stream');
+var browserify  = require('browserify');
 
 gulp.task('simonSays', function() {
   console.log("simon says");
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/browser.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js']})
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
 });
